263fdd2f3124c01526b42e40165589dfbefbee80
Simranjeet Singh <35461304+simranjeet07@users.noreply.github.com>
2018-07-22 17:51:34 +0530

    Update settings.py

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^)xpu7m-5-2vm$6h&4t$g2$2!r^5s5**hbsuueb-2lo_61+2f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['simranjeet07.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['tweetme.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
